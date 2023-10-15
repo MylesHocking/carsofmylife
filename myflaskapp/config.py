47ad010731fc1c0a5646f3d65991abb50b450140
MylesHocking <myleshocking@gmail.com>
2023-10-15 17:42:04 +0100

    allowed

--- myflaskapp/config.py
+++ myflaskapp/config.py
@@ -2,4 +2,4 @@ import os
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:J4sp3rw00@localhost/carsofmylife')
 
-ALLOWED_ORIGINS = "http://localhost:3000"
+ALLOWED_ORIGINS = os.environ.get('ALLOWED_ORIGINS', 'http://localhost:3000')
